688200a8392d3b51c0f309eafd0585ab3dad6959
Lokprakash-babu <lokprakash100@gmail.com>
2020-08-16 06:29:42 +0530

    Updates

---
+++ precache-manifest.bde019b308478a0d363a5c4fcfe96ebe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "746e0c9e8ad9529312c75587801ada99",
+    "url": "/monsters-Rolodox/index.html"
+  },
+  {
+    "revision": "9fc0bd692eec7935ec13",
+    "url": "/monsters-Rolodox/static/css/main.ba2127bb.chunk.css"
+  },
+  {
+    "revision": "e26f3b49cda25c8c1e51",
+    "url": "/monsters-Rolodox/static/js/2.36960f10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-Rolodox/static/js/2.36960f10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fc0bd692eec7935ec13",
+    "url": "/monsters-Rolodox/static/js/main.0ba9a02f.chunk.js"
+  },
+  {
+    "revision": "5088cd72e5dca3cf6075",
+    "url": "/monsters-Rolodox/static/js/runtime-main.6bb1e01e.js"
+  }
+]);
